98c1ffd73c908010cff7b574e41790a6e6f4c73b
Paul <lolol2222@protonmail.com>
2023-05-12 20:30:56 +0200

    react click events

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,9 +5,45 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("click!");
+  };
+
+  const handleClickTwo = (name) => {
+    console.log(`${name} was clicked!`);
+  };
+
+  const handleClickThree = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button
+        onClick={handleClick}
+        className="bg-gray-500 w-96 p-3 block ml-auto mr-auto"
+      >
+        Click it
+      </button>
+
+      <button
+        onClick={() => {
+          handleClickTwo("John");
+        }}
+        className="bg-gray-700 w-96 p-3 block mt-3 ml-auto mr-auto"
+      >
+        Click it
+      </button>
+
+      <button
+        onClick={(e) => {
+          handleClickThree(e);
+        }}
+        className="bg-gray-900 w-96 p-3 block mt-3 ml-auto mr-auto"
+      >
+        Click it
+      </button>
     </main>
   );
 };
